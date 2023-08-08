5dd2ae98c6559a09efdc96a75890029b7040e53d
Rachelle Burgos <rachelle.burgoss@gmail.com>
2023-08-08 01:02:40 -0400

    oops

--- normalized_tables.sql
+++ normalized_tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE Country (
     country_id INT AUTO_INCREMENT PRIMARY KEY,
     country_code VARCHAR(4),
     country_name VARCHAR(32),
-    country_population UNSIGNED INT,
+    country_population INT,
     country_currency VARCHAR(10),
     country_description VARCHAR(500)
 );
@@ -19,7 +19,7 @@ CREATE TABLE City (
     FOREIGN KEY (country_fk) REFERENCES Country(country_id),
     city_name VARCHAR(32),
     city_region VARCHAR(32),
-    city_population UNSIGNED INT,
+    city_population INT,
     city_description VARCHAR(500)
 );
 
@@ -32,7 +32,7 @@ CREATE TABLE Attraction (
     FOREIGN KEY (city_fk) REFERENCES City(city_id),
     attraction_name VARCHAR(32),
     attraction_description VARCHAR(500),
-    attraction_rating UNSIGNED SMALLINT,
+    attraction_rating SMALLINT,
     attraction_photo_url VARCHAR(500)
 );
 
@@ -45,7 +45,7 @@ CREATE TABLE Accommodation (
     FOREIGN KEY (city_fk) REFERENCES City(city_id),
     accommodation_name VARCHAR(32),
     accommodation_address VARCHAR(500),
-    accommodation_rating UNSIGNED SMALLINT,
+    accommodation_rating SMALLINT,
     accommodation_average_price DECIMAL(10,2),
     accommodation_website_url VARCHAR(500)
 );
@@ -60,7 +60,7 @@ CREATE TABLE Restaurant (
     restaurant_name VARCHAR(32),
     restaurant_address VARCHAR(500),
     restaurant_cuisine_type VARCHAR(32),
-    restaurant_rating UNSIGNED SMALLINT,
+    restaurant_rating SMALLINT,
     restaurant_website_url VARCHAR(500)
 );
 
@@ -107,7 +107,7 @@ CREATE TABLE Review (
     attraction_id INT,
     FOREIGN KEY (user_fk) REFERENCES User(user_id),
     FOREIGN KEY (attraction_fk) REFERENCES Attraction(attraction_id),
-    review_rating UNSIGNED SMALLINT,
+    review_rating SMALLINT,
     review_text VARCHAR(500)
 );
 
